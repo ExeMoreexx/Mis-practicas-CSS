4a795284f5f60b9ef55df00630b35d7b279b9ed3
exemoreexx <miguelaguirre7712@gmail.com>
2022-11-01 19:15:50 -0600

    Modificado

--- js/25_RickAndMortyAPI.js
+++ js/25_RickAndMortyAPI.js
@@ -7,14 +7,14 @@ const apiRick = async (pagina) => {
   data.results.map((item) => {
     divItem = document.createElement("div");
     divItem.innerHTML = `
-    <div class="card" style="width: 18rem;">
-    <img src="${item.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${item.name}</h5>
-    <p class="card-text"><b>Estado: </b>${item.status}<br><b>Especie: </b>${item.species}</br><b>Genero: </b>${item.gender}</p>
-    </div>
-    </div>
-    `;
+          <div class="card" style="width: 18rem;">
+          <img src="${item.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">${item.status}<br>${item.species}</br>${item.gender}</br></p>
+          </div>
+          </div>
+          `;
     divRes.appendChild(divItem);
   });
 };
